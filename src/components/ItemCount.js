64ef7ac7b1da82f10b780cf7b7626816ab8f3866
Fernando.Baumann <fgbaumann@icloud.com>
2021-09-18 17:52:01 -0300

    cuarto commit

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -8,8 +8,20 @@ const ItemCount = ({stock, initial, onAdd}) => {
     return(
         <>
         <h2>Cantidad: {contador}</h2>
-        <button onClick={()=>{ setContador(contador+1)}}> Aumentar </button> 
-        <button onClick={()=>{ setContador(contador-1)}}> Disminuir </button>
+        <button onClick={()=>{ 
+            if(contador<stock){
+            setContador(contador+1)}
+            else{
+                alert("La cantidad no puede exceder el stock");
+            }}}> Aumentar </button> 
+
+        <button onClick={()=>{
+            if(contador>0){
+
+           setContador(contador-1)
+        }else{
+           alert("El contador no puede ser negativo")}}}> Disminuir </button>
+           
         <button onClick={()=>{ alert("Compra confirmada")}}> Confirmar </button>
 
         </>
